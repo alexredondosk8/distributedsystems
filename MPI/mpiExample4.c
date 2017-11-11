147549436fb42f108c43c317de037ced5f8d25ed
alexredondosk8 <alexredondosk8@gmail.com>
2017-11-11 05:31:14 +0000

    Tarea MPI

--- MPI/mpiExample4.c
+++ MPI/mpiExample4.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-#include <iostream>
-#include <time.h>
-#include <stdlib>
 #include "mpi.h"
-
-using namespace std;
 
 int main(int argc,char *argv[]){
 	long i, maxVal;
 	long start, end;
 	int size, rank;
-	double x, y, rand_nums;
-	long sumTotal, sum, circle, cirTotal; // utilizamos Long para que no se desborde la operación
+	long sumTotal, sum; // utilizamos Long para que no se desborde la operación
 
-	rand_nums = create_rand_nums(nums_elements_per_proc);
+	maxVal = 100000;
 
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -23,20 +17,18 @@ int main(int argc,char *argv[]){
 	if(rank == 1){
 		// El inicio del segmento en la variable 'start', el fin del segmento
 		// la variable 'end'
-		start = 0; 
-		end = 1e3;
-		circle = 0;
+		start = 1; 
+		end = 20000;
+		 sum = 0;
     	for(i=start; i<= end; i++){
-    		x = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
-    		y = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
-    		if (x*x+y*y <= 1.0) circle ++;
+    		sum = sum +i;
     	}
 	}
 	else if (rank == 2){
 		// El inicio del segmento en la variable 'start', el fin del segmento
 		// la variable 'end'
-		start = 0;
-		end = 1e4;
+		start = 20001;
+		end = 40000;
 		sum = 0;
     	for(i=start; i<= end; i++){
     		sum = sum +i;
@@ -45,8 +37,8 @@ int main(int argc,char *argv[]){
 	else if (rank == 3){
 		// El inicio del segmento en la variable 'start', el fin del segmento
 		// la variable 'end'
-		start = 0;
-		end = 1e5;
+		start = 40001;
+		end = 60000;
 		sum = 0;
     	for(i=start; i<= end; i++){
     		sum = sum +i;
@@ -55,8 +47,8 @@ int main(int argc,char *argv[]){
 	else if (rank == 4){
 		// El inicio del segmento en la variable 'start', el fin del segmento
 		// la variable 'end'
-		start = 0;
-		end = 1e6;
+		start = 60001;
+		end = 80000;
 		sum = 0;
     	for(i=start; i<= end; i++){
     		sum = sum +i;
@@ -65,28 +57,8 @@ int main(int argc,char *argv[]){
 	else if (rank == 5){
 		// El inicio del segmento en la variable 'start', el fin del segmento
 		// la variable 'end'
-		start = 0;
-		end = 1e7;
-		sum = 0;
-    	for(i=start; i<= end; i++){
-    		sum = sum +i;
-    	}
-	}
-	else if (rank == 6){
-		// El inicio del segmento en la variable 'start', el fin del segmento
-		// la variable 'end'
-		start = 0;
-		end = 1e8;
-		sum = 0;
-    	for(i=start; i<= end; i++){
-    		sum = sum +i;
-    	}
-	}
-	else if (rank == 7){
-		// El inicio del segmento en la variable 'start', el fin del segmento
-		// la variable 'end'
-		start = 0;
-		end = 1e9;
+		start = 80001;
+		end = 100000;
 		sum = 0;
     	for(i=start; i<= end; i++){
     		sum = sum +i;
@@ -94,14 +66,14 @@ int main(int argc,char *argv[]){
 	}
 	// Utilice la funcion 'MPI_Reduce' para guardar en la variable 
 	// 'sumTotal' la suma parcial de todos las tareas
-	MPI_Reduce(&circle, &cirTotal, 1, MPI_LONG, MPI_SUM, 0,
+	MPI_Reduce(&sum, &sumTotal, 1, MPI_LONG, MPI_SUM, 0,
            MPI_COMM_WORLD);
     
     if (rank == 0){
-	printf ("\nTotal: %ld\n",cirTotal);
+	printf ("\nTotal: %ld\n",sumTotal);
 	}
 
 	MPI_Finalize();
 	
 	return 0;
-}
+} 
